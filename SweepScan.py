98c2c004fd3c954ec9634cfe620dae2a51497515
Bryce Chappell <103286819+CaptainGhost945@users.noreply.github.com>
2023-07-28 18:50:59 -0400

    Update SweepScan.py

--- SweepScan.py
+++ SweepScan.py
@@ -3,6 +3,14 @@ import time
 import sys
 import os
 import nmap
+#This is a personal project I wanted to put some of my Python skills to the test, expirement with networking libraries, 
+#and gain a better understanding of nmap. The concept for this tool is simple yet effective: look for all of the IPs on 
+#a subnet, scan the ones that are online, and then store each IP scan into a file the user can come back to later. 
+#It was a fun little project that does its job!
+#This tool was made for ethical and legal uses only. Any other such use is not approved, unethical, and possibly illegal. 
+#The tool is required to run as root or by using sudo. 
+#This is because the nmap scan runs and grabs operating system information of the live hosts on the network, 
+#which requires `sudo` or root to run. An example case of running this tool is `sudo python3 SweepScan.py 192.168.10.10`. 
 
 # Initialize global variables that the program will be working with
 live_hosts = []
@@ -106,7 +114,7 @@ def scan_live_hosts():
                 nm.scan(ip,'1-65535', "-A -T3 -O") # This line tells nmap to perform an aggressive scan with three threads (T4 could result in missing something) and grabbing the OS information
                 print("[-] Scan complete!") # Tell the user a scan is complete
             except:
-                 print("[!] Error: An error occurred scanning the hosts!")
+                 print("[!] Error: An error occurred scanning the hosts! Try running with sudo or as root!")
                  exit(6)
             try:
                  formatted_results = format_nmap_scan_results(nm[ip]) # Call the format_nmap_scan_results function to format the results from the scan
